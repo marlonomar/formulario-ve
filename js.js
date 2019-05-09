255d22665559b91e380babb178c8c1f62c262416
Garcia <marlon.omar.g.garcia@accenture.com>
2019-05-09 15:17:32 -0300

    estados funcionando

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  
-  const ninos = ()=>{
+const ninos = ()=>{
     $("#si").click(function(){
         $(".ninos").show();
         $(".ninas").show();
@@ -11,10 +10,10 @@ $(document).ready(function(){
         $(".ninas").hide();
     })
   }
-
   ninos();
 
-  const pasaporte = ()=>{
+
+const pasaporte = ()=>{
       $("#pasaporte").click(function(){
            let pass = $(this).val();
           if(pass == 'true'){
@@ -24,60 +23,87 @@ $(document).ready(function(){
           }
       })
   }
-
   pasaporte();
 
- /**mostar estados y parroquias */
 
-  const lugares =(lugar1,lugar2)=>{
-      $(lugar1).on('click',function(){
-            $(lugar2).show();
-            let estadove = $(this).val();
-            localStorage.setItem('estadove',estadove);
-      })
-  }
-   /**venezuela*/
-  lugares("#estado-ve",".municipio");
-  lugares("#municipio-ve",".parroquia");
-
-  /**brasil*/
-  lugares("#estado-br",".municipio-br");
-  lugares("#municipio-br",".parroquia-br")
-
-  /** -------------------------------*/
+const ocutarCiudades = ()=>{
+    const lugares =(lugar1,lugar2)=>{
+        $(lugar1).on('click',function(){
+              $(lugar2).show();
+        })
+    }
   
-  venezuelaES();
-    function venezuelaES(){
-        $.ajax({
-            url:"./venezuela.json",
-            data:"GET",
-            dataType:"JSON",
-            success:function(pais){
-                for (let i = 0; i < pais.length; i++) {
-                    $("#estado-ve").append(`<option value='${pais[i].estado}' index='${pais[i].id_estado}'>${pais[i].estado}</option>`);  
-                }
-            }
-        });
-    }
-    
-  /** */
+    lugares("#estado-ve",".municipio");
+    lugares("#municipio-ve",".parroquia");
+    lugares("#estado-br",".cidade-br");
+}
+ocutarCiudades();
 
-  brasilES();
-    function brasilES(){
-        $.ajax({
-            url:"./brasil.json",
-            data:"GET",
-            dataType:"JSON",
-            success:function(pais){
-                console.log(pais)
-               for (let i = 0; i < pais[0].estados.length; i++) {
-                $("#estado-br").append(`<option value='${pais[0].estados[i].nome}' sigla='${pais[0].estados[i].sigla}'>${pais[0].estados[i].nome}</option>`); 
-               }
+const callJson = ()=>{
+    venezuelaES();
+function venezuelaES(){
+    $.ajax({
+        url:"./venezuela.json",
+        data:"GET",
+        dataType:"JSON",
+        success:(pais)=>{
+           
+            for (let i = 0; i < pais.length; i++) {
+                $("#estado-ve").append(`<option value='${pais[i].id_estado}'>${pais[i].estado}</option>`);  
             }
-        });
-    }
-    
- 
+
+            $(document).on('change',"#estado-ve",function(){
+                var municipio = $(this).val();
+                $("#municipio-ve").empty();
+                for (let i = 0; i < pais[municipio -1].municipios.length; i++) {
+                    $("#municipio-ve").append(`<option value='${[i]}'>${pais[municipio -1].municipios[i].municipio}</option>`);  
+                }
+            })
+
+            $(document).on('change',"#municipio-ve",function(){
+                let estados = parseInt($("#estado-ve").val());
+                let municipio = parseInt($("#municipio-ve").val());
+                $("#parroquia-ve").empty();
+                for (let i = 0; i < pais[estados -1].municipios[municipio].parroquias.length; i++) {
+                    $("#parroquia-ve").append(`<option value='${[i]}'>${pais[estados -1].municipios[municipio].parroquias[i]}</option>`);  
+                }
+            })
+
+        }
+    });
+}
+
+
+/** */
+
+brasilES();
+function brasilES(){
+    $.ajax({
+        url:"./brasil.json",
+        data:"GET",
+        dataType:"JSON",
+        success:(pais)=>{
+           for (let i = 0; i < pais[0].estados.length; i++) {
+            $("#estado-br").append(`<option value='${i}' sigla='${pais[0].estados[i].sigla}'>${pais[0].estados[i].nome}</option>`); 
+           }
+
+           $(document).on('change',"#estado-br",function(){
+            var cidade = parseInt($(this).val())
+            $("#cidade-br").empty();
+            for (let i = 0; i < pais[0].estados[cidade].cidades.length; i++) {
+                $("#cidade-br").append(`<option value='${[i]}'>${pais[0].estados[cidade].cidades[i]}</option>`);  
+            }
+
+        })
+        }
+    });
+}
+
+
+
+
+}
+callJson();
 
 
 
